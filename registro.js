21eff05d5b3332b61275d40746182f346db099a1
Harp09 <hebert.rivera@vallegrande.edu.pe>
2022-12-16 21:16:35 -0500

    URl

--- registro.js
+++ registro.js
@@ -1,4 +1,4 @@
-const url = "http://34.196.73.242:3000/api/pedido/";
+const url = "http://44.212.135.107:3000/api/pedido/";
 let resultados = '';
 const formArticulo = document.querySelector("form");
 const userped = document.getElementById("USERPED");
