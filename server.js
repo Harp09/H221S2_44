4f0167d0da7e2627b5e3374303c781ca6b110f0b
Harp09 <hebert.rivera@vallegrande.edu.pe>
2022-12-16 22:37:07 -0500

    Probando

--- server.js
+++ server.js
@@ -1,15 +1,75 @@
-const http= require('http');
-const express = require('express');
-const app = express();
+var express = require('express');
+  
 
-//recursos(permite que el css conecte con html)
-app.use(express.static(__dirname+'/'));
+var app = express();
 
-//Enrutamiento
-app.get('/',(req,res) => {
-    res.sendFile("/home/ubuntu/landing/H221S2_44")
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + "/index.html");
 });
 
+app.get('/login', function (req, res) {
+	res.sendFile(__dirname + "/login.html");
+});
 
-app.listen(3000);
-console.log('server on port 3000');
+const puerto = process.env.PUERTO || 3000;
+
+app.listen(puerto, function () {
+  console.log("Servidor funcionando en puerto: " + puerto);
+});
+
+//Recursos
+app.use(express.static(__dirname+'/'));
+
+
+
+
+
+
+var mysql = require("mysql");
+
+var cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+ 
+//Verficar si esta informacion es correcta de acuerdo a tu localhost
+var conexion = mysql.createConnection({
+  host: "44.212.135.107",
+  user: "hebert",
+  password: "12345",
+  database: "formulario",
+});
+ 
+//Verificar si la conexion a base de datos fue exitosa ,de lo contrario te devolvera un error
+conexion.connect(function (error) {
+    if (error) {
+      throw error;
+    } else {
+      console.log("Conexión exitosa");
+    }
+  });
+
+  
+
+app.post("/api/pedido", (req, res) => {
+	let data = {
+    	userped: req.body.USERPED,
+    	emausped: req.body.EMAUSPED,
+    	celusped: req.body.CELUSPED,
+    	foodped: req.body.FOODPED,
+    	msgped: req.body.MSGPED
+	};
+	let sql = "INSERT INTO PEDIDO SET ?";
+	conexion.query(sql, data, function (error, results) {
+  	if (error) {
+    	throw error;
+  	} else {
+    	console.log(data);
+    	res.send(data);
+  	}
+	});
+  });
