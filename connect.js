69dd88997a3e741d2f0b81c4086ec160f98444c9
Harp09 <hebert.rivera@vallegrande.edu.pe>
2022-12-16 22:47:42 -0500

    Formulario

--- connect.js
+++ connect.js
@@ -1,23 +1,33 @@
 var express = require('express');
- 
- 
+  
+
 var app = express();
 
 
-app.get('/', (req, res) => {
-//res.send('Hello World with express!')
-res.sendFile("/home/H221S2_/index.html")
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000);
-console.log('Server on port 3000');
+app.get('/login', function (req, res) {
+	res.sendFile(__dirname + "/login.html");
+});
+
+const puerto = process.env.PUERTO || 3000;
+
+app.listen(puerto, function () {
+  console.log("Servidor funcionando en puerto: " + puerto);
+});
 
 //Recursos
 app.use(express.static(__dirname+'/'));
 
 
 
-//De acuerdo a lo que hemos instalado
+
+
 
 var mysql = require("mysql");
 
@@ -25,45 +35,41 @@ var cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(__dirname + '/'));
-
+ 
 //Verficar si esta informacion es correcta de acuerdo a tu localhost
 var conexion = mysql.createConnection({
-host: "44.212.135.107",
-user: "hebert",
-password: "12345",
-database: "db_landing_page"
+  host: "44.212.135.107",
+  user: "hebert",
+  password: "12345",
+  database: "formulario",
 });
-
+ 
 //Verificar si la conexion a base de datos fue exitosa ,de lo contrario te devolvera un error
 conexion.connect(function (error) {
-if (error) {
-  console.log(error)
-  throw error;
-} else {
-  console.log("Conexión exitosa");
-}
-});
+    if (error) {
+      throw error;
+    } else {
+      console.log("Conexión exitosa");
+    }
+  });
 
+  
 
-
-
-//El contrato entre el servidor y el cliente para permitir la inserción de registros en la tabla
 app.post("/api/pedido", (req, res) => {
-let data = {
-	userped: req.body.USERPED,
-	emausped: req.body.EMAUSPED,
-	celusped: req.body.CELUSPED,
-	foodped: req.body.FOODPED,
-	msgped: req.body.MSGPED
-};
-let sql = "INSERT INTO CONTACTO SET ?";
-conexion.query(sql, data, function (error, results) {
-  if (error) {
-	throw error;
-  } else {
-	console.log(data);
-	res.send(data);
-  }
-});
-});
+	let data = {
+    	userped: req.body.USERPED,
+    	emausped: req.body.EMAUSPED,
+    	celusped: req.body.CELUSPED,
+    	foodped: req.body.FOODPED,
+    	msgped: req.body.MSGPED
+	};
+	let sql = "INSERT INTO PEDIDO SET ?";
+	conexion.query(sql, data, function (error, results) {
+  	if (error) {
+    	throw error;
+  	} else {
+    	console.log(data);
+    	res.send(data);
+  	}
+	});
+  });
